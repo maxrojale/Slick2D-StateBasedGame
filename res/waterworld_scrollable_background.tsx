d2cd9f5ef8664c5347fa753da85cbbde12d92b09
pickman1 <34723284+pickman1@users.noreply.github.com>
2018-04-02 14:46:03 +0200

    Add files via upload

---
+++ res/waterworld_scrollable_background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="waterworld_scrollable_background" tilewidth="64" tileheight="64" tilecount="120" columns="15">
+ <image source="../../../Pictures/zeichnungen/waterworld_scrollable_background.png" width="960" height="540"/>
+</tileset>
